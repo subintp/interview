be1f4688ee6bbc971b46ea2af11f7c8bdbfd6ad6
kamal namdeo <kamal.github@gmail.com>
2016-04-06 08:44:41 +0530

    add response status in all actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,25 @@
 class PostsController < ApplicationController
-  #protect_from_forgery with: :null_session
   skip_before_filter :verify_authenticity_token
-  #respond_to :json
+  before_action      :set_post, only: :update
+  ##
+  # Returns a list of all posts
+  #
+  # GET /posts
+  #
+  # params: 
+  #   Not Required
   def index
     @posts = Post.all
-    #byebug
     respond_to do |format|
       format.json {render json: @posts.to_json(:except => [:created_at, :updated_at])}
-      #format.html 
     end
   end
 
-  #/posts/2
+  # GET /posts/:id
+  # Returns the post with specified id
+
+  # params: :id
+
   def show
     @post = Post.find_by(id: params[:id])
     respond_to do |format|
@@ -24,21 +32,32 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     if @post.save
       respond_to do |format|
-        format.json {render json: @post.to_json(:except => [:created_at, :updated_at])}
+        format.json {render json: @post.to_json(:except => [:created_at, :updated_at]), status: 201}
       end
     else
       respond_to do |format|
-        format.json {render json: @post.errors}
+        format.json {render json: @post.errors, status: 422}
       end
     end
   end
 
   def update
-    @post.update(post_params)
-    render json: @post, status: :created
+    if @post.update(post_params)
+      respond_to do |format|
+        format.json {render json: @post.to_json(:except => [:created_at, :updated_at]), status: 200}
+      end
+    else
+      respond_to do |format|
+        format.json {render json: @post.errors, status: 422}
+      end
+    end
   end
 
   private
+
+  def set_post
+    @post = Post.find_by(id: params[:id])
+  end
 
   def post_params
     params.permit(:title, :body)
