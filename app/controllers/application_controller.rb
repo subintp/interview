a0de5eb4a6484b267e948014d38d5bd895c851fd
subintp <subin.tp@chillr.in>
2016-04-26 14:23:12 +0530

    Wraps every action in rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+  protect_from_forgery with: :null_session
+  around_action :wrap_in_rescue
+
+  private
+
+  def wrap_in_rescue
+    begin
+      yield
+    rescue  => e
+      head :bad_request
+    end
+  end
+
 end
