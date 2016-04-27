85b479958f7b487ecf1bb78045e063427e3b109f
subintp <subin.tp@chillr.in>
2016-04-28 00:26:01 +0530

    Adds handlebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,11 @@
 //= require jquery_ujs
 //= require underscore
 //= require backbone
+//= require handlebars.runtime
+//= require_tree ./templates
+//= require_directory ./backbone
+//= require_tree ./backbone/models
+//= require_tree ./backbone/collections
+//= require_tree ./backbone/views
 //= require turbolinks
 //= require_tree .
